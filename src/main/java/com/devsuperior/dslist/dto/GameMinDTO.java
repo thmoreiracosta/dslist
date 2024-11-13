f297098b0602f8a47eaf296b68ae090ce3c7fab6
Thiago Costa <thiagomoreiracosta@gmail.com>
2024-11-13 13:52:52 -0300

    projections

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	
@@ -14,6 +15,15 @@ public class GameMinDTO {
 	
 	public GameMinDTO() {		
 	}
+	
+	public GameMinDTO(GameMinProjection projection) {		
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
+	}
+
 
 	public GameMinDTO(Game entity) {		
 		id = entity.getId();
